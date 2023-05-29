305ae55b2e5ade1fc8faba055324caf933b8ae2e
PGVlasov <PGVlasov@users.noreply.github.com>
2023-05-29 13:23:51 +0300

    fix grammatical error

--- frontend/src/pages/people/people.js
+++ frontend/src/pages/people/people.js
@@ -3,7 +3,7 @@ import { People } from "../../components/people/people"
 export const PeoplePage = () => {
   return (
     <div>
-      <p>This is the Peaple Page</p>
+      <p>This is the People Page</p>
       <People />
     </div>
   )
